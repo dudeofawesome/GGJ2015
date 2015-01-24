e5fd71c4dd224fa6c1cb997ae57f4ecb5a5f4263
Louis Orleans <louis@0rleans.com>
2015-01-24 09:45:08 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+GGJ2014
+=======
+
+This is the repo for our 2014 Global Game Jam project.
---
+++ bin/Assets/VRplayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRplayerController : MonoBehaviour {
+
+	[SerializeComponent] private GameObject leftEye;
+	[SerializeComponent] private GameObject rightEye;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
