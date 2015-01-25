ddadcf5f67a2b519f35db60443c9420a722a4012
Louis Orleans <louis@0rleans.com>
2015-01-24 22:34:49 -0800

    getting ready to pull from DK

--- bin/Assets/Scripts/GameEngine.cs
+++ bin/Assets/Scripts/GameEngine.cs
@@ -5,13 +5,15 @@ public class GameEngine : MonoBehaviour {
 
 	[SerializeField] private GameObject player;
 
-	enum World {EARTH:0, MARS:1, ISS:2};
+	enum World {EARTH, MARS, ISS};
 	[SerializeField] private World currentLocation = World.MARS;
+	private bool changedWorldThisFrame = true;
 
 	private WorldState[] worldStates = WorldState[3];
 
 	private float lastSwitched;
-	[SerializeField] private float timePerWorld = 30;
+	[SerializeField] private float TIMEPERWORLD = 30;
+	private readonly float WORLDCHANGEHINTTIME = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +22,18 @@ public class GameEngine : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (changedWorldThisFrame) {
+			if (worldStates[currentLocation].firstVisit) {
+				// World Setup
+
+				worldStates[currentLocation].firstVisit = false;
+			} else {
+				// Restart all AI etc for the world
+			}
+
+			changedWorldThisFrame = false;
+		}
+
 		switch (currentLocation) {
 			case World.MARS :
 
@@ -32,10 +46,13 @@ public class GameEngine : MonoBehaviour {
 				break;
 		}
 
-		if (Time.time - lastSwitched > timePerWorld) {
+		if (Time.time - lastSwitched > TIMEPERWORLD - WORLDCHANGEHINTTIME)
+		if (Time.time - lastSwitched > TIMEPERWORLD) {
 			lastSwitched = Time.time;
+			changedWorldThisFrame = true;
 			worldStates[currentLocation].playerTransform = player.transform;
 			worldStates[currentLocation].playerHealth = player.GetComponent<VRplayerController>().health;
+			currentLocation = currentLocation.next;
 		}
 	}
 }
--- bin/Assets/Scripts/WorldState.cs
+++ bin/Assets/Scripts/WorldState.cs
@@ -4,4 +4,5 @@ using System.Collections;
 public class WorldState {
 	public Transform playerTransform;
 	public int playerHealth;
+	public bool firstVisit = true;
 }
